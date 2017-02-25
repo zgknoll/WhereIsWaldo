611d7faf58a1861e799ecc3c2913f8f2b2002f29
cathens <cathens@iastate.edu>
2017-02-24 20:50:17 -0600

    Added OpenCV Library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Claudog" << std::endl;
+    cout << "Hello, World!" << std::endl;
+
+    cout << "Claudog" << std::endl;
+
     return 0;
 }
 
 int addition(int x, int y) {
-    return x+y;
+    return x + y;
 }
